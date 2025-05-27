7bb54cce35c61cecc7782d871a0bbd917f3919f8
Jennifer <lopezgomezjennifer50@hotmail.com>
2025-05-26 22:11:13 -0500

    pagina principal

--- ProyectoAnalisisNumerico/urls.py
+++ ProyectoAnalisisNumerico/urls.py
@@ -17,9 +17,14 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'index.html')
 
 
 urlpatterns = [
+    path('', index, name='index'),
     path('admin/', admin.site.urls),
     path('capitulo1/', include('Capitulo1.urls')),
     path('capitulo2/', include('Capitulo2.urls')),
